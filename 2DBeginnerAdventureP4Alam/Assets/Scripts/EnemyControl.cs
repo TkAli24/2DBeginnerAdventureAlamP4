c829daede94f22584defb4b7ee76e8233e229286
TkAli24 <741712@student.dusd.net>
2024-11-01 11:43:48 -0700

    finish animation
    did animation the player ch did not move properly.

--- 2DBeginnerAdventureP4Alam/Assets/Scripts/EnemyControl.cs
+++ 2DBeginnerAdventureP4Alam/Assets/Scripts/EnemyControl.cs
@@ -10,13 +10,16 @@ public class EnemyControl : MonoBehaviour
 
     Rigidbody2D rigidbody2d;
     float timer;
-    int direction = 1; 
+    int direction = 1;
 
+    Animator animator;
+        
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
         timer = changeTime;
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -36,10 +39,14 @@ public class EnemyControl : MonoBehaviour
         Vector2 position = rigidbody2d.position;
         if(vertical)
         {
+            animator.SetFloat("Move X", 0);
+            animator.SetFloat("Move Y", direction);
             position.y = position.y + speed  * Time.deltaTime * direction;
         }
         else
         {
+            animator.SetFloat("Move x", direction);
+            animator.SetFloat("Move Y", 0);
             position.x = position.x + speed * Time.deltaTime * direction;
         }
         
--- 2DBeginnerAdventureP4Alam/Assets/Scripts/RubyController.cs
+++ 2DBeginnerAdventureP4Alam/Assets/Scripts/RubyController.cs
@@ -6,7 +6,7 @@ public class RubyController : MonoBehaviour
 {
     public float speed = 3.0f;
     public int maxHealth = 5;
-    public float timeInvincible = 2;
+    public float timeInvincible = 2.0f;
    public int health { get { return currentHealth; } }
     int currentHealth;
 
@@ -16,13 +16,15 @@ public class RubyController : MonoBehaviour
     Rigidbody2D rigidbody2d;
     float horizontal;
     float vertical;
+    Animator animator;
+    Vector2 lookDirection = new Vector2(1, 0);
 
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
         currentHealth = maxHealth;
-        
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -31,9 +33,24 @@ public class RubyController : MonoBehaviour
         horizontal = Input.GetAxis("Horizontal"); 
         vertical = Input.GetAxis("Vertical");
 
-        if (isInvincible) 
+        Vector2 move = new Vector2(horizontal, vertical);
+
+        if(!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y,0.0f))
         {
-            isInvincible = false;
+            lookDirection.Set(move.x, move.y);
+            lookDirection.Normalize();
+        }
+        animator.SetFloat("Look X", lookDirection.x);
+        animator.SetFloat("Look Y", lookDirection.y);
+        animator.SetFloat("speed", move.magnitude);
+           
+        if (isInvincible)
+        {
+            inInvincibleTime = Time.deltaTime;
+            if(inInvincibleTime < 0)
+            {
+                isInvincible = false;
+            }
         }
 
 
@@ -49,6 +66,8 @@ public class RubyController : MonoBehaviour
     {
         if(amount < 0)
         {
+            animator.SetTrigger("Hit");
+
             if(isInvincible)
             {
                 return;
